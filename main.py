212b02043b76328e6537475528bb5510a628cf8b
Qieerb <qieerbushe7@gmail.com>
2018-02-06 16:37:50 +0000

    servo tracker update
    integrated motor functions into main.py

--- main.py
+++ main.py
@@ -4,20 +4,39 @@ import network
 import machine
 from umqtt.simple import MQTTClient
 
+#TODO list
+#define a mutable class with all state parameters
+#wifi connection temporarily commented out for motor testing 
+#---------------------------------------------------------------
 #declarations
+#communication
 i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4), freq=100000)
 CLIENT_ID = machine.unique_id()
 BROKER_ADDRESS = '192.168.0.10'
-#value(1) -> turn off
-#value(0) -> turn on
-red_led = machine.Pin(0, machine.Pin.OUT)
-blue_led = machine.Pin(2, machine.Pin.OUT)
 ap_if = network.WLAN(network.AP_IF)
 sta_if = network.WLAN(network.STA_IF)
 client = MQTTClient(CLIENT_ID, BROKER_ADDRESS)
 
+#leds
+#value(1) -> turn off
+#value(0) -> turn on
+red_led = machine.Pin(0, machine.Pin.OUT)
+blue_led = machine.Pin(2, machine.Pin.OUT)
+
+#motor
+servo = machine.PWM(machine.Pin(12),freq=50)
+INITIAL_DUTY = 57
+STEP_SIZE = 8
+direction = True
+
 #---------------------------------------------------------------
-#functions to be moved to seperate files
+# class tracker_state:
+#     def __init__(self, direction, duty, lux):
+#         self.direction = direction
+#         self.duty = duty
+#         self.lux = lux
+
+#---------------------------------------------------------------
 def connect_wifi(name, password):
     #setup connection
     print ("connecting to " + name + "...")
@@ -38,10 +57,10 @@ def connect_wifi(name, password):
     blue_led.value(0)
     #MQTT setup
     client.connect()
-    client.set_callback(print_msg)
+    client.set_callback(msg_print)
     client.subscribe('esys/JEDI/Server/')
-
-def calc_lux(ch0, ch1):
+#---------------------------------------------------------------
+def sensor_calc(ch0, ch1):
     if ch0 == 0:
         return float('nan')
 
@@ -58,26 +77,7 @@ def calc_lux(ch0, ch1):
     elif ratio>1.3:
         return 0
 
-def blink_msg(topic, msg):
-    if msg == b'Hello ESP8266':
-        blue_led.value(0)
-        time.sleep_ms(500)
-        blue_led.value(1)
-
-def print_msg(topic, msg):
-    print(msg)
-
-
-#---------------------------------------------------------------
-#power up
-i2c.writeto_mem(0x39,0xA0,bytearray([0x03]))
-red_led.value(1)
-blue_led.value(1)
-
-connect_wifi('EEERover','exhibition')
-#---------------------------------------------------------------
-#main loop
-while True:
+def sensor_read():
     #read data
     channel0 = i2c.readfrom_mem(0x39,0xAC,2)
     channel0 = int.from_bytes(channel0,'little')
@@ -86,24 +86,80 @@ while True:
     channel1 = int.from_bytes(channel1,'little')
 
     #calc output
-    lux = calc_lux(channel0, channel1)
+    lux = sensor_calc(channel0, channel1)
     print (lux)
+    return lux
+#---------------------------------------------------------------
+#motor move should change direction when one edge is reached
+#duty cycle range may need to be re-tested
+#a mutable class should be passed around this group of functions
+def motor_move(current_duty, direction):
+    if direction:
+        current_duty += STEP_SIZE
+    else:
+        current_duty -= STEP_SIZE
+    if current_duty < 21:
+        current_duty = 21
+    if current_duty > 120:
+        current_duty = 120
+    servo.duty(current_duty)
+    return current_duty
+
+def motor_track(current_duty, direction, old_lux):
+    if 21 <= current_duty <= 120:
+        time.sleep_ms(500)
+        current_duty = motor_move(current_duty, direction)
+        new_lux = sensor_read()
+        time.sleep_ms(500)
+        if new_lux <= old_lux:
+            direction = not direction
+        else:
+            pass
+        return (current_duty, direction, new_lux)
+#---------------------------------------------------------------
+def msg_blink(topic, msg):
+    if msg == b'Hello ESP8266':
+        blue_led.value(0)
+        time.sleep_ms(500)
+        blue_led.value(1)
+
+def msg_print(topic, msg):
+    print(msg)
+
+#---------------------------------------------------------------
+#power up
+i2c.writeto_mem(0x39,0xA0,bytearray([0x03]))
+
+#initialisation
+red_led.value(1)
+blue_led.value(1)
+servo.duty(INITIAL_DUTY)
+current_duty = INITIAL_DUTY
+lux = sensor_read()
+
+#connect_wifi('EEERover','exhibition')
+#---------------------------------------------------------------
+#main loop
+while True:
 
     #read physcial input
 
     #control motor
-
+    (current_duty, direction, lux) = motor_track(current_duty, direction, lux)
     #check if still connected (donno if this code works yet)
-    if (not sta_if.isconnected()):
-        blue_led.value(1)
-        connect_wifi('EEERover','exhibition')
+    #if (not sta_if.isconnected()):
+    #    blue_led.value(1)
+    #    connect_wifi('EEERover','exhibition')
 
     #send message
-    payload = json.dumps({'name':'luminous flux','data':lux})
-    client.publish('esys/JEDI/', bytes(payload,'utf-8'))
+    #payload = json.dumps({'name':'luminous flux',
+    #                        'brightness':lux,
+    #                        'direction':direction,
+    #                        'duty': current_duty})
+    #client.publish('esys/JEDI/', bytes(payload,'utf-8'))
 
     #read message
-    client.check_msg()
+    #client.check_msg()
 
-    time.sleep_ms(800)
+    time.sleep_ms(100)
 #---------------------------------------------------------------
